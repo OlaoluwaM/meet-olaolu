6ed392b47fd8cc187c2b805a8b7ebfec917b0568
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2020-01-20 14:05:50 -0600

    dd

--- src/components/App.js
+++ src/components/App.js
@@ -1,109 +1,106 @@
 import React from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
 import '../index.css';
-import { spring2 } from '../utils/motionObj';
+import { motion } from 'framer-motion';
+import {
+  mainVariant,
+  Variant,
+  ItemVariant,
+  TitleVariant,
+  ContentVariant,
+} from '../utils/motionObj';
+import { WaveSVG } from './SVGbackground';
+import { FaReact, FaHtml5, FaCss3, FaJs } from 'react-icons/fa';
+const PageContainer = styled(motion.main).attrs({
+  className: 'App',
+})`
+  background: var(--color1);
+  position: relative;
+
+  & > svg {
+    display: block;
+  }
+`;
+
+const TextContainer = styled(motion.div)`
+  padding-top: 50px;
+  background: transparent;
+  width: 100%;
+  height: 50vh;
+  padding-left: 5%;
+`;
 
 const Title = styled(motion.h1)`
   font-family: var(--font-header);
-  font-size: 6rem;
+  font-size: 5.2rem;
   font-weight: 500;
   margin-top: 0;
-  margin-bottom: 1.2rem;
+  margin-bottom: 1rem;
 `;
 
 const Content = styled(motion.p)`
   font-family: var(--font-body);
   font-weight: 500;
-  width: 52%;
-  font-size: 1.4rem;
+  width: 53%;
+  font-size: 1.3rem;
   margin-top: 0;
   margin-bottom: 1rem;
+  line-height: 2rem;
 `;
 
 const Item = styled(motion.li)`
-  list-style-type: square;
-  font-size: 1.2rem;
+  font-size: 1.3rem;
   font-family: var(--font-body);
   margin: 10px;
+  display: flex;
+  flex-direction: row;
+  width: fit-content;
+  & > svg {
+    margin-right: 20px;
+    fill: #dbd8e3;
+  }
 `;
-
-const mainVariant = {
-  visible: {
-    opacity: 1,
-    transition: {
-      when: 'beforeChildren',
-      staggerChildren: 0.3,
-    },
-  },
-  hidden: {
-    opacity: 0,
-  },
-};
-
-const Variant = {
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      when: 'beforeChildren',
-      staggerChildren: 0.2,
-      delayChildren: 0.4,
-    },
-  },
-  hidden: {
-    opacity: 0,
-    y: -80,
-  },
-};
-
-const ItemVariant = {
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { ...spring2 },
-  },
-  hidden: {
-    opacity: 0,
-    y: -80,
-  },
-};
-
-const TitleVariant = {
-  visible: { opacity: 1, y: 0, transition: { ...spring2 } },
-  hidden: { opacity: 0, y: -80 },
-};
-
-const ContentVariant = {
-  visible: { opacity: 1, y: 0, transition: { ...spring2 } },
-  hidden: { opacity: 0, y: -80 },
-};
 
 function App() {
   return (
-    <motion.main
-      className="App"
-      variants={mainVariant}
-      initial="hidden"
-      animate="visible">
-      <Title variants={TitleVariant}>Olaoluwa Mustapha</Title>
-      <Content variants={ContentVariant}>
-        A 16 year old who loves programming and everything tech. I am a Front
-        end developer with 3+ years of experience. I love to program and create
-        for the web, but I am also intrigued by the tech outside the browser
-        environment. Here are some of the technologies I use:
-      </Content>
-      <motion.ul
-        style={{ marginTop: '0' }}
-        variants={Variant}
-        initial="hidden"
-        animate="visible">
-        <Item variants={ItemVariant}>HTML</Item>
-        <Item variants={ItemVariant}>CSS</Item>
-        <Item variants={ItemVariant}>JavaScript</Item>
-        <Item variants={ItemVariant}>React</Item>
-      </motion.ul>
-    </motion.main>
+    <PageContainer>
+      <TextContainer variants={mainVariant} initial="hidden" animate="visible">
+        <Title variants={TitleVariant}>Olaoluwa Mustapha</Title>
+        <Content variants={ContentVariant}>
+          A 16 year old who loves programming and everything tech. I am a Front
+          end developer with 3+ years of experience. I love to program and
+          create for the web, but I am also intrigued by the tech outside the
+          browser environment. Here are some of the technologies I use:
+        </Content>
+        <motion.ul
+          style={{ listStyle: 'none', marginTop: '0' }}
+          variants={Variant}
+          initial="hidden"
+          animate="visible">
+          <Item variants={ItemVariant}>
+            <FaHtml5 />
+            HTML
+          </Item>
+          <Item variants={ItemVariant}>
+            <FaCss3 />
+            CSS
+          </Item>
+          <Item variants={ItemVariant}>
+            <FaJs />
+            JavaScript
+          </Item>
+          <Item variants={ItemVariant}>
+            <FaReact />
+            React
+          </Item>
+        </motion.ul>
+        <Content style={{ marginBottom: '0' }} variants={ContentVariant}>
+          I am currently available and look forward to working with you
+          remotely.
+        </Content>
+      </TextContainer>
+      <WaveSVG color="#fff" />
+    </PageContainer>
   );
 }
 
--- src/utils/motionObj.js
+++ src/utils/motionObj.js
@@ -16,3 +16,55 @@ export const tween = {
   duration: 0.2,
   ease: 'easeIn',
 };
+
+export const mainVariant = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren',
+      staggerChildren: 0.3,
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+export const Variant = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      when: 'beforeChildren',
+      staggerChildren: 0.2,
+      delayChildren: 0.4,
+      staggerDirection: -1,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: -80,
+  },
+};
+
+export const ItemVariant = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { ...spring2 },
+  },
+  hidden: {
+    opacity: 0,
+    y: -80,
+  },
+};
+
+export const TitleVariant = {
+  visible: { opacity: 1, y: 0, transition: { ...spring2 } },
+  hidden: { opacity: 0, y: -80 },
+};
+
+export const ContentVariant = {
+  visible: { opacity: 1, y: 0, transition: { ...spring2 } },
+  hidden: { opacity: 0, y: -80 },
+};
